4586dacf575097bac30f07d892d37b17e1b5ff09
acefei <acefeilxy@gmail.com>
2020-11-04 19:08:43 +0800

    Test useCallback

--- src/App.js
+++ src/App.js
@@ -31,16 +31,29 @@ const Level2 = React.memo(({ children }) => {
   );
 }, areEqual);
 
-const Level3 = React.memo(() => {
+const Level3 = React.memo(({ onSubmit }) => {
   // const { dataSource } = useDataSource();
   // console.log("Level3 render.", dataSource);
   console.log("Level3 render.");
-  return <h2>Level3 show up</h2>;
+  return (
+    <>
+      <h2>Level3 show up</h2>
+      <button onClick={onSubmit}>submit</button>
+    </>
+  );
 });
 
 export default function App() {
   const { dataSource, setDataSource } = useDataSource();
   console.log("App", dataSource);
+  const onSubmit1 = () => {
+    console.log("Click Submit");
+  };
+
+  const onSubmit2 = React.useCallback(() => {
+    console.log("Click Submit Callback");
+  }, []);
+
   return (
     <div className="App">
       <button
@@ -52,7 +65,7 @@ export default function App() {
       </button>
       <Level1>
         <Level2>
-          <Level3 />
+          <Level3 onSubmit={onSubmit2} />
         </Level2>
       </Level1>
     </div>
